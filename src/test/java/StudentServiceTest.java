9210dcff254eab2fbc6a632f227e4a6a93d90465
khailqd81 <khailqd81@gmail.com>
2023-08-10 10:22:26 +0700

    Add unit tests for student service

---
+++ src/test/java/StudentServiceTest.java
@@ -0,0 +1,53 @@
+
+import entity.Student;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import repository.StudentRepository;
+import service.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class StudentServiceTest {
+
+    StudentService studentService;
+
+    StudentRepository studentRepository = mock(StudentRepository.class);
+
+    @Before
+    public void initialize() {
+        this.studentService = new StudentService();
+        this.studentService.studentRepository = studentRepository;
+    }
+
+    @Test
+    public void findAll() {
+        Assert.assertTrue(studentService.findAll().isEmpty());
+    }
+
+    @Test
+    public void add() {
+        Student st = mock(Student.class);
+        when(studentRepository.add(st)).thenReturn(1);
+        Assert.assertEquals(1, studentService.add(st));
+    }
+
+    @Test
+    public void findById() {
+        Student st = mock(Student.class);
+        when(studentRepository.findById(1)).thenReturn(st);
+        Assert.assertEquals(st, studentService.findById(1));
+    }
+
+    @Test
+    public void findByName() {
+        Student st = mock(Student.class);
+        List<Student> list = new ArrayList<>();
+        list.add(st);
+        when(studentRepository.findByName("Khai")).thenReturn(list);
+        Assert.assertEquals(list, studentService.findByName("Khai"));
+    }
+}
